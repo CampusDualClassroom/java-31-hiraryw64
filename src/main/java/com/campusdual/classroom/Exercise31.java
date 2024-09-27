d7fedaa5d39bf624463a47e711669b5df49e0d8b
hiraryw64 <hiraryw64@gmail.com>
2024-09-27 14:24:55 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,24 @@
 package com.campusdual.classroom;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        try {
+            File file = new File("src/main/resources/lorem.txt");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
 
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.err.println("No se pudo leer el archivo.\n" + e.getMessage());
+        }
     }
 }
